8e1fca5e87d9fd019e1cae2340fca90e714849cb
Robert Chambers <rmchambers@gmail.com>
2022-12-01 07:00:57 +0100

    added materialui

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,29 @@
+import * as React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+import { CacheProvider } from "@emotion/react";
+import theme from "../config/theme";
+import createEmotionCache from "../config/createEmotionCache";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>{process.env.HOME_TITLE || 'My Digital Home Binder'}</title>
+                <meta name="description" content={'Digital House Binder for ' + (process.env.HOME_TITLE || 'My Digital Home Binder')} />
+                <link rel="icon" href="/favicon.ico" />
+                <meta name={'viewport'} content={'minimum-scale=1, initial-scale=1, width=device-width'} />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
-
-export default MyApp
