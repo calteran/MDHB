c356ff00220e4d5f0a5910c3134c28f7348343bd
Robert Chambers <rmchambers@gmail.com>
2022-12-09 09:57:02 +0100

    started implementing next-auth before realising this is stupid

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { CssBaseline } from "@mui/material";
 import { CacheProvider } from "@emotion/react";
 import theme from "../config/theme";
 import createEmotionCache from "../config/createEmotionCache";
+import {SessionProvider} from "next-auth/react";
 
 
 const clientSideEmotionCache = createEmotionCache();
@@ -13,17 +14,19 @@ export default function MyApp(props) {
     const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
     return (
-        <CacheProvider value={emotionCache}>
-            <Head>
-                <title>{process.env.HOME_TITLE || 'My Digital Home Binder'}</title>
-                <meta name="description" content={'Digital House Binder for ' + (process.env.HOME_TITLE || 'My Digital Home Binder')} />
-                <link rel="icon" href="/favicon.ico" />
-                <meta name={'viewport'} content={'minimum-scale=1, initial-scale=1, width=device-width'} />
-            </Head>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </CacheProvider>
+        <SessionProvider session={pageProps.session}>
+            <CacheProvider value={emotionCache}>
+                <Head>
+                    <title>{process.env.HOME_TITLE || 'My Digital Home Binder'}</title>
+                    <meta name="description" content={'Digital House Binder for ' + (process.env.HOME_TITLE || 'My Digital Home Binder')} />
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta name={'viewport'} content={'minimum-scale=1, initial-scale=1, width=device-width'} />
+                </Head>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </CacheProvider>
+        </SessionProvider>
     );
 }
